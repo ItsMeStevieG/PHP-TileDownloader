0c300563bd1102d3409b6906c7941baa8f3e3e76
Stevie G <ItsMeStevieG@users.noreply.github.com>
2019-03-03 12:12:58 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,44 @@
 <?php
-set_time_limit(0); //Allow Script to Execute for as long as it needs
-ini_set('display_errors', TRUE); //enabled for debugging
-error_reporting(E_ALL ^ E_NOTICE); //Omit Notices but show all other errors
+set_time_limit(0);
+ini_set('display_errors', TRUE);
+error_reporting(E_ALL ^ E_NOTICE);
 
-require_once("TileDownloader.class.php"); //Require Main class
-$td=new TileDownloader(); //Instantiate main class
+require_once("TileDownloader.class.php");
+$td=new TileDownloader();
 
 //Tile Service Config
-$CONFIG[0]["tileServiceName"]="Six Aerial"; //Friendly Tile Service Name
-$CONFIG[0]["tileService"]="http://maps.six.nsw.gov.au/arcgis/rest/services/public/NSW_Imagery/MapServer/tile"; //Tile Service URL without trailing slash /
-$CONFIG[0]["fileExtension"]="";
-$CONFIG[0]["destPath"]="tiles/six-aerial"; // local storage directory Without trailing slash /
-$CONFIG[0]["zoomStart"]="10"; //Start Zoom Level
-$CONFIG[0]["zoomEnd"]="11"; //End Zoom Level
-$CONFIG[0]["MinLng"]="151.36617122624398"; //SouthWest Lng
-$CONFIG[0]["MaxLng"]="152.6313316236804"; //NorthWest Lng
-$CONFIG[0]["MinLat"]="-28.249257948470415"; //NorthEast Lat
-$CONFIG[0]["MaxLat"]="-29.426018708820767"; //SouthWest Lat
+$CONFIG[0]["tileServiceName"]="Six Aerial";
+$CONFIG[0]["tileService"]="http://maps.six.nsw.gov.au/arcgis/rest/services/public/NSW_Imagery/MapServer/tile/{z}/{y}/{x}";
+$CONFIG[0]["destPath"]="tiles/six-aerial/"; //Include Trailing slash /
+$CONFIG[0]["zoomStart"]="1";
+$CONFIG[0]["zoomEnd"]="10";
+$CONFIG[0]["BBox"]="151.901436,-29.140467,152.131119,-28.961290"; //Format SouthWestLng,SouthWestLat,NorthEastLng,NorthEastLat (Left,Bottom,Right,Top)
+
+$CONFIG[1]["tileServiceName"]="OSM";
+$CONFIG[1]["tileService"]="https://{switch:a,b,c}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+$CONFIG[1]["destPath"]="tiles/osm/"; //Include Trailing slash /
+$CONFIG[1]["zoomStart"]="1";
+$CONFIG[1]["zoomEnd"]="12";
+$CONFIG[1]["BBox"]="151.901436,-29.140467,152.131119,-28.961290"; //Format SouthWestLng,SouthWestLat,NorthEastLng,NorthEastLat (Left,Bottom,Right,Top)
+
 
 ######################### DONT MODIFY BELOW THIS LINE ##############################
 foreach($CONFIG as $key=>$val)
 {
 	echo("Downloading Tiles for ".$val['tileServiceName']." (Zoom Levels ".$val["zoomStart"]." - ".$val["zoomEnd"].")<br>");
 
-	$td->setTileService($val["tileService"]); //Set Service URL
-	$td->setDestPath($val["destPath"]); //Set Destination Path
-	$td->setMinZoom($val["zoomStart"]); //Set Zoom Start
-	$td->setMaxZoom($val["zoomEnd"]); //Set Zoom End
-	$td->setMinLng($val["MinLng"]); //Set Min Longitude
-	$td->setMaxLng($val["MaxLng"]); //Set Max Longitude
-	$td->setMinLat($val["MinLat"]); //Set Min Latitude
-	$td->setMaxLat($val["MaxLat"]); //Set Max Latitude
+	$td->setTileService($val["tileService"]);
+	$td->setDestPath($val["destPath"]);
+	$td->setMinZoom($val["zoomStart"]);
+	$td->setMaxZoom($val["zoomEnd"]);
+	$td->setBBox($val["BBox"]);
 	$time_start = microtime(true); 
-	$td->Download($val['fileExtension']); //Start Map Downloading
+	$td->Download();
 	$time_end = microtime(true);
 	$execTime = formatSeconds($time_end - $time_start);
 	echo("Downloaded ".$td->downImageCount." tile(s) in $execTime<br>###############################################<br>");
 	//Reset Tile Download Counter
-	$td->downImageCount=0; //Reset Tile Download Count
+	$td->downImageCount=0;
 }
 
 function formatSeconds($iSeconds) {
